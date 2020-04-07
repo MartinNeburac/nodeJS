7ac114ccd3b64340191eed5ce9b3368bc2fa6dd4
MartinNeburac <58069937+MartinNeburac@users.noreply.github.com>
2020-04-07 20:57:41 +0200

    saveProc1.5

--- app.js
+++ app.js
@@ -27,9 +27,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+
 
 // error handler
 app.use(function(err, req, res, next) {
